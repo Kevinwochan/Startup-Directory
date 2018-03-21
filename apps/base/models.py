4772f34f3050b64eb50067533c8479c29170c01e
Unknown <kevinwochan@gmail.com>
2018-03-22 00:26:21 +1100

    Adding first model to DB

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,1 +1,7 @@
 """Base models"""
+from django.db import models
+
+class company (models.Model):
+    compane_name = models.CharField(max_length=200)
+    company_description = models.CharField(max_length=1000)
+

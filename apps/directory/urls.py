b233f0724e2e397be88e6a201146db454096e44c
Unknown <kevinwochan@gmail.com>
2018-03-22 19:16:08 +1100

    added Profile Page Template

--- apps/directory/urls.py
+++ apps/directory/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, path
 
 from .views import index
+B
 
 urlpatterns = [
     url(r'',index,name='index'),
+    path('<int:comapany_id/',profile,name='profile'),
 ]
--- apps/directory/views.py
+++ apps/directory/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def profile (request, company_id):
+    #comapny_dict = getCompany_info(company_id);
+    return render(request,'proflePage.html',company_dict)

995be10a99b877f1ad800e6051ec7925e6d9b2c2
Unknown <kevinwochan@gmail.com>
2018-03-26 21:27:17 +1100

    List companies in template
    Template tags added for directiory list
    Irrelevant models removed

--- apps/directory/models.py
+++ apps/directory/models.py
@@ -6,15 +6,4 @@ class Company (models.Model):
     submission_date = models.DateTimeField('submission date')
     industries = models.CharField(max_length=200)
     website_url = models.CharField(max_length=200)
-
-class Question (models.Model):
-   question_text = models.CharField(max_length=200)
-   def __str__(self):
-       return self.question_text
-    
-class Answer (models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer_text = models.CharField(max_length=200)
-    def __str__(self):
-       return self.answer_text
-    
+   

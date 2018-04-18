79be4b021ad049ce8a2751f3942c931a564fce5b
Unknown <kevinwochan@gmail.com>
2018-04-18 19:43:00 +1000

    Update Templating for images

--- apps/directory/views.py
+++ apps/directory/views.py
@@ -4,28 +4,42 @@ from django.conf.urls.static import static
 
 # display list of companies sorted by date added
 def index(request, sortby):
-    if isValid_field (sortby) :
+    if isValid_field (sortby):
         companies = Company.objects.order_by(sortby)
-    else : 
+    else: 
         companies = Company.objects.order_by('submission_date')
+        
     return render(request,"index.html",{'companies':companies})
-
+    
+    
 def profile (request, company_id):
 #   feitches a dict of a comapny 
     company_dict = get_object_or_404(Company,pk=company_id)
 
     return render(request,'proflePage.html',{'company_dict':company_dict})
 
+
 # modify displayed information
 def columns(request):
-
     return
 
-# 
+
 def statistics (request):
     return
+    
+    
 # displays a list of comapnies, founders, 
 def search (searched_string):
     return
+    
 
+def isValid_field ( field ):
+
+    try:
+        Company._meta.get_field(field)
+        
+    except: 
+        return 0
+    
+    return 1
 

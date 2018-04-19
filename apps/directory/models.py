4ed9910390af475f0b8165c783db24611b2747fd
Unknown <kevinwochan@gmail.com>
2018-04-19 15:35:29 +1000

    Added Default values to Company model

--- apps/directory/models.py
+++ apps/directory/models.py
@@ -1,13 +1,15 @@
+from django.utils import timezone
+from datetime import date
 from django.db import models
 
 class Company (models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    founded_date = models.DateField()
-    founders = models.CharField(max_length=200)
-    stage = models.CharField(max_length=200)
-    submission_date = models.DateTimeField()
-    industries = models.CharField(max_length=200)
-    website = models.URLField(max_length=200)
-    email = models.EmailField(max_length=200)
-    logo = models.ImageField(upload_to='logos')
+    name = models.CharField(max_length=200, default='undefined')
+    description = models.CharField(max_length=200, default='undefined')
+    founded_date = models.DateField(default=date.today)
+    founders = models.CharField(max_length=200,default="undefined")
+    stage = models.CharField(max_length=200,default="undefined")
+    submission_date = models.DateTimeField(default=timezone.now)
+    industries = models.CharField(max_length=200,default="undefined")
+    website = models.URLField(max_length=200,default="undefined")
+    email = models.EmailField(max_length=200,default="undefined")
+    logo = models.ImageField(upload_to='logos',default="/logo/undefined.png")

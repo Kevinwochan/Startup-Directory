b46570231d2fd72726e12d70db82e1ab4d4dff8a
Unknown <kevinwochan@gmail.com>
2018-04-22 02:46:17 +1000

    Named index path in directory app, removed all url paths in base

--- apps/directory/urls.py
+++ apps/directory/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
-    url(r'^$(?P<sortby>)$',index),
+    url(r'^$(?P<sortby>)$',index, name='index'),
     url(r'^sortby=(?P<sortby>)$',index),
     url(r'^search=(?P<searched_string>)$', search),
     url('^profile/(?P<company_id>\d+)$', profile),

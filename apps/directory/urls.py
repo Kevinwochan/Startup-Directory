a753d27bb890a5d179cf447feceb1af44a27890b
Unknown <kevinwochan@gmail.com>
2018-04-22 23:31:26 +1000

    fixed sorting with tighter URL routing

--- apps/directory/urls.py
+++ apps/directory/urls.py
@@ -5,7 +5,11 @@ from .views import *
 app_name='directory'
 urlpatterns = [
     url(r'^$(?P<sorting_string>)$',index, name='home'),
-    url(r'^sortby=(?P<sorting_string>[a-z&-_]+)$',index, name='index'),
-    url(r'^search=(?P<searched_string>)$', search),
+   # url(r'^sortby=(?P<sorting_string>[-a-z&_]+)$',index, name='index'),
+    url(r'^sortby=(?P<sorting_string>-?name)$',index, name='index'),
+    url(r'^sortby=(?P<sorting_string>-?industries)$',index, name='index'),
+    url(r'^sortby=(?P<sorting_string>-?year_founded)$',index, name='index'),
+    url(r'^sortby=(?P<sorting_string>-?stage)$',index, name='index'),
+    url(r'^sortby=(?P<sorting_string>-?funding)$',index, name='index'),
     url('^profile/(?P<company_id>\d+)$', profile),
 ]

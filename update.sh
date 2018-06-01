ab0a91b8e79212eefe839618bb28aa5b7376f9a4
Unknown <kevinwochan@gmail.com>
2018-06-02 02:36:20 +1000

    Clean wipe of entire database on each sync

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ $(cd ~/Startupdirectory;
  git reset --hard;
  git pull;
  pipenv install --dev;
+ rm -f *.sqlite3;
+pipenv run python manage.py migrate --sync-db;
 pipenv run python manage.py makemigrations;
 pipenv run python manage.py migrate;
 pipenv run python manage.py shell < downloadStartups.py)

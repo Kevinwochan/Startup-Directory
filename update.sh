6d6956dfce5e64bf5b8bad3a7806c0cb8cc80308
Unknown <kevinwochan@gmail.com>
2018-06-02 02:26:14 +1000

    fixing typo

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 #!/bin/sh
 
 $(cd ~/Startupdirectory;
+ git reset --hard;
  git pull;
  pipenv install --dev;
 pipenv run python manage.py makemigrations;
 pipenv run python manage.py migrate;
-pipenv run python maNage.py shell < downloadStartups.py)
+pipenv run python manage.py shell < downloadStartups.py)

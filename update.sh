c1b360d0ee1956a0bf2247f6258c6b2474614299
Unknown <kevinwochan@gmail.com>
2018-06-02 02:03:15 +1000

    added sheets integration with update script

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$(cd ~/startupdirectory; git pull; pipenv install --dev;pipenv run python manage.py migrate;)
+$(cd ~/startupdirectory; git pull; pipenv install --dev;pipenv run python manage.py migrate;pipenv rn python manage.py shell < downloadStartups.py)

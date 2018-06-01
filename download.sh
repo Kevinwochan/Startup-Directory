15564393b912adbd27b3f40d32ee795e5bafb85c
Unknown <kevinwochan@gmail.com>
2018-06-02 02:21:56 +1000

    Hotfixes to scipts

--- download.sh
+++ download.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # download default logo image
-$(curl https://upload.wikimedia.org/wikipedia/commons/9/93/No-logo.svg > public/media/logos/undefined.svg)
+$(mkdir ./public/media)
+$(mkdir ./public/media/logos/)
+$(curl https://upload.wikimedia.org/wikipedia/commons/9/93/No-logo.svg > ./public/media/logos/undefined.svg)
 # download navbar logo
-$(curl https://textbook.ventures/wp-content/uploads/2018/03/TBV-Logo-Horizontal-56PX.png > public/static/img/textbook-ventures.png)
+$(mkdir ./public/static)
+$(mkdir ./public/static/img)
+$(curl https://textbook.ventures/wp-content/uploads/2018/03/TBV-Logo-Horizontal-56PX.png > ./public/static/img/textbook-ventures.png)
 # download tbv icon
-$(curl https://textbook.ventures/wp-content/uploads/2018/03/cropped-TBV-Logo-Icon-Only-Dark-BG@2x-1-32x32.png> public/static/img/textbook-ventures-icon.png)
+$(curl https://textbook.ventures/wp-content/uploads/2018/03/cropped-TBV-Logo-Icon-Only-Dark-BG@2x-1-32x32.png> ./public/static/img/textbook-ventures-icon.png)
--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+#!/bin/sh
 
-$(cd ~/startupdirectory; git pull; pipenv install --dev;pipenv run python manage.py migrate;pipenv rn python manage.py shell < downloadStartups.py)
+$(cd ~/Startupdirectory;
+ git pull;
+ pipenv install --dev;
+pipenv run python manage.py makemigrations;
+pipenv run python manage.py migrate;
+pipenv run python maNage.py shell < downloadStartups.py)

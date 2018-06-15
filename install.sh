6b5b94e80510ac769e3c898e68e799690fbfae93
Unknown <kevinwochan@gmail.com>
2018-06-15 14:46:17 +1000

    improving terraforming automation

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-# download default logo image
+# Update files to the latest repo
+$(chmod +x *.sh) 
+$(./update.sh)
+# Sanity checking firewall permisions
+$(sudo ufw allow 'Apache Full')
+# Restart Apache
+$(sudo systemctl restart apache2)
+# Download default logo image
 $(mkdir ./public/media)
 $(mkdir ./public/media/logos/)
 $(curl https://upload.wikimedia.org/wikipedia/commons/9/93/No-logo.svg > ./public/media/logos/undefined.svg)
--- update.sh
+++ update.sh
@@ -5,7 +5,11 @@ $(cd ~/startupdirectory;
  git pull;
  pipenv install --dev;
  rm -f *.sqlite3;
-pipenv run python manage.py migrate --sync-db;
+pipenv run python manage.py migrate --run=syncdb;
 pipenv run python manage.py makemigrations;
 pipenv run python manage.py migrate;
-pipenv run python manage.py shell < downloadStartups.py)
+pipenv run python manage.py shell < downloadStartups.py;
+chmod 664 ~/startupdirectory/*.sqlite3;
+sudo chown :www-data ~/startupdirectory;
+sudo chown :www-data ~/startupdirectory/*.sqlite3;
+) > /home/ubuntu/update.log

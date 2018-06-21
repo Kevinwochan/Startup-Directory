f1b93006c389197e83c0e56ed2f3c5f7f6930354
Unknown <kevinwochan@gmail.com>
2018-06-22 05:02:32 +1000

    Solving permission error

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ pipenv run python manage.py migrate --run-syncdb;
 pipenv run python manage.py makemigrations;
 pipenv run python manage.py migrate;
 pipenv run python manage.py shell < downloadStartups.py;
-chmod 664 ~/startupdirectory/*.sqlite3;
+sudo chmod +rw ~/startupdirectory/*.sqlite3;
 sudo chown :www-data ~/startupdirectory;
 sudo chown :www-data ~/startupdirectory/*.sqlite3;
